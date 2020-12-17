d1ed0fdd901355c4de9bb9db8f16ea211844625d
Henry <vinsen1109@gmail.com>
2020-12-17 21:23:45 +0800

    add shop action & view

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -4,5 +4,24 @@ class ShopsController < ApplicationController
     @shop = Shop.new
   end
 
+  def create
+    @shop = Shop.new(shop_params)
+
+    if @shop.save
+      redirect_to root_path, notice: "註冊成功"
+    else
+      render :new, notice: "註冊失敗"
+    end
+  end 
+
+
+
+
+
+  private
+  def shop_params
+    params.require(:shop).permit(:name, :tel)
+  end
+
 
 end
