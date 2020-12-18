bfdde8d020f3afb0f5cbb675c3229c216e0e57c8
Henry <vinsen1109@gmail.com>
2020-12-18 21:48:52 +0800

    add validate

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
   has_many :order_item
   has_many :sub_order, through: :order_item
+
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :quantity, presence: true
+  validates :price, presence: true
+
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,8 @@
 class Shop < ApplicationRecord
   belongs_to :user
   has_many :products
+
+  validates :name, presence: true
+  validates :tel, presence: true
+
 end
