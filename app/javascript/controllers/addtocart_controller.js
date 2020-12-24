ae2d4f2be467b197c54062d3bedc6cf8e3612ffe
JiaMingXie <jiamingxie@JiaMingXiedeMacBook-Pro.local>
2020-12-24 19:25:58 +0800

    finish cart total price

--- app/javascript/controllers/addtocart_controller.js
+++ app/javascript/controllers/addtocart_controller.js
@@ -1,6 +1,16 @@
 import { Controller } from "stimulus"
 import magicRails from '@rails/ujs'
 
+function updateCartTotal() {
+  let carttotal = document.querySelector('.cart_total')
+  let items = document.querySelectorAll('.item_total_price')
+
+  let totalprice = 0
+  for (let i = 0; i< items.length; i++) {
+    totalprice += Number(items[i].textContent)
+  }
+  carttotal.textContent = totalprice
+}
 
 export default class extends Controller {
   static targets = [ "amount", "additem", "totalprice" , 'price']
@@ -21,9 +31,19 @@ export default class extends Controller {
     }
   }
 
+  changequantity(e) {
+    this.numberValue = Number(this.amountTarget.value)
+  }
+
+
+
+
   numberValueChanged() {
-    this.amountTarget.value = this.numberValue
-    this.totalpriceTarget.textContent = Number(this.priceTarget.textContent) * Number(this.amountTarget.value)
+    if (this.numberValue !== NaN || this.numberValue != '') {
+      this.amountTarget.value = this.numberValue
+      this.totalpriceTarget.textContent = Number(this.priceTarget.textContent) * Number(this.amountTarget.value)  
+    }
+    updateCartTotal()
   }
 
 
