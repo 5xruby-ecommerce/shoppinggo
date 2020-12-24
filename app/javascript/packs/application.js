7f5ceef4e705217b2978964b26d910adb65e0bd8
barpa00 <barpamonster@gmail.com>
2020-12-24 11:35:14 +0800

    addReact

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,5 @@ require("scripts")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
 import "controllers"
 import "@fortawesome/fontawesome-free/css/all.css"
