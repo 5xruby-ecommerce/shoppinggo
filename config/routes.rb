32b521d0360c0e3f38a6c363e352f3e67dd1dd19
kyu__yui <chiuyuf61139@gmail.com>
2020-12-29 20:31:54 +0800

    can upload img to localhost

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: "users/registrations" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: "user/registrations" }
   root 'pages#home'
   
    devise_scope :user do
