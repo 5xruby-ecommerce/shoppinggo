ecc55b79d4760d21a6fae16ccbea100d5784eae3
Henry <vinsen1109@gmail.com>
2020-12-31 02:53:26 +0800

    rescue render not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@
 class ApplicationController < ActionController::Base
 
   include CartsHelper
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  private
+  def render_not_found
+    render :file => '/public/404.html', layout: false, :status => 404
+  end
+
 end
