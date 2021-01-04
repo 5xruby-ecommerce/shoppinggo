353ef45043bfc7f6117650e91b468b62f1710653
kyu__yui <chiuyuf61139@gmail.com>
2021-01-04 14:50:04 +0800

    remove_column :shops, :image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_023726) do
+ActiveRecord::Schema.define(version: 2021_01_04_064854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 2021_01_03_023726) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image"
     t.index ["user_id"], name: "index_shops_on_user_id"
   end
 
