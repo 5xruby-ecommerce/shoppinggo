0b697a172f9150f186112104ceb242611d7f88f1
Henry <vinsen1109@gmail.com>
2021-01-05 18:15:39 +0800

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_085904) do
+ActiveRecord::Schema.define(version: 2021_01_05_093430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2021_01_05_085904) do
     t.string "content"
     t.integer "price"
     t.integer "quantity"
-    t.string "shop_id"
+    t.integer "shop_id"
     t.string "references"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
