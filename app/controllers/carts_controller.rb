a0e1213faa218b2f0361fb3cfec2bb29318c2472
Henry <vinsen1109@gmail.com>
2021-01-08 01:14:52 +0800

    change cart session method & delete useless file

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,9 +4,9 @@ class CartsController < ApplicationController
   def add_item
     if current_user
       product = Product.find(params[:id])
-      quantity = params[:amount].to_i
-      # quantity = JSON.parse(params.keys.filter{|i| i[/.amount/]}.first)["amount"].to_i
-      current_cart.add_item(product.id, quantity, product.name, product.price)
+      # quantity = params[:amount].to_i
+      quantity = JSON.parse(params.keys.filter{|i| i[/.amount/]}.first)["amount"].to_i
+      current_cart.add_item(product.id, quantity)
       session[:cartgo] = current_cart.serialize
       redirect_to root_path, notice: '已加入購物車'
     else
@@ -63,21 +63,44 @@ class CartsController < ApplicationController
   end
 
   private
-
   def create_order
     if current_user
       order = Order.new(user: current_user, sum: current_cart.total_price)
-      sub_order = order.sub_orders.new()
 
-      current_cart.items.each do |cart_item|
-        sub_order.order_items.new(
-          product_id: cart_item.product_id,
-          quantity: cart_item.quantity,
-          price: cart_item.product_price
-        )
+      # {
+      #   shopA_id => {
+      #     productAA_id => productAA,
+      #     productAB_id => productAB
+      #   },
+      #   shopB_id => {
+      #     productBA_id => productBA,
+      #     productBB_id => productBB
+      #   },
+      # }
+      products_all = Product.includes(:shop).
+        where(id: current_cart.product_ids).
+        reduce({}) do |rs, product|
+          rs[product.shop_id] ||= {}
+          rs[product.shop_id][product.id] = product
+          rs
+        end
+
+      products_all.each do |(shop_id, products)|
+        items = current_cart.items.filter { |item| item.product_id.in?(products.keys) }
+        sum = items.sum(&:total_price)
+        order.sub_orders.new(sum: sum)
       end
-    order.save!
-    order
+
+      # current_cart.items.each do |cart_item|
+      #   sub_order.order_items.new(
+      #     product_id: cart_item.product_id,
+      #     quantity: cart_item.quantity,
+      #     price: cart_item.product_price
+      #   )
+      # end
+      byebug
+      order.save!
+      order
     else
       redirect_to new_user_session_path
     end
@@ -91,7 +114,7 @@ class CartsController < ApplicationController
       'PaymentType' => 'aio',
       'TotalAmount' => current_cart.total_price,
       'TradeDesc' => '123',
-      'ItemName' => current_cart.product_names,
+      'ItemName' => current_cart.items.first.product.name,
       'ReturnURL' => 'http://localhost:3000/carts/checkout',
       'ClientBackURL' => 'http://localhost:3000/carts/checkout',
       'ChoosePayment' => 'Credit',
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,10 +1,9 @@
 class CartItem
-  attr_reader :product_id, :quantity, :product_name, :product_price
-  def initialize(product_id, quantity = 1, product_name, product_price)
+  attr_reader :product_id, :quantity
+
+  def initialize(product_id, quantity = 1)
     @product_id = product_id
     @quantity = quantity
-    @product_name = product_name
-    @product_price = product_price
   end
 
   def increament(n = 1)
@@ -12,15 +11,10 @@ class CartItem
   end
 
   def product
-    @product = Product.find_by(id: @product_id)
-  end
-
-  def sub_order_sum
-    # @product = Product
+    @product ||= Product.find_by(id: product_id)
   end
 
   def total_price
-    @product = Product.find_by(id: product_id)
-    @quantity.to_i * @product.price
+    quantity.to_i * product.price
   end
 end
