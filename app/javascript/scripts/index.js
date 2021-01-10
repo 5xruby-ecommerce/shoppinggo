6eeef9d72548337ef2d44945ffdfaed4691f70aa
kyu__yui <chiuyuf61139@gmail.com>
2021-01-10 23:43:59 +0800

    use stimulus get data-favorite-id

--- app/javascript/scripts/index.js
+++ app/javascript/scripts/index.js
@@ -1,5 +1,10 @@
 import "./datetimepicker.js"
 import "./select2.js"
+<<<<<<< HEAD
 // import './shopitem.js';
 import './cart.js'
 import './swiper.js'
+=======
+
+//require('./products')
+>>>>>>> f8c8287... use stimulus get data-favorite-id
