1079e31cfc1f9c6bc51a4358f3d6a32652b82723
ooo <ooo@gmail.com>
2021-01-11 19:43:20 +0800

    productnotfinished

--- app/javascript/scripts/index.js
+++ app/javascript/scripts/index.js
@@ -2,3 +2,4 @@ import "./datetimepicker.js"
 import "./select2.js"
 // import './shopitem.js';
 import './cart.js'
+import './swiper.js'
---
+++ app/javascript/swiper/index.js
@@ -0,0 +1,5 @@
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// Swiper.use([Navigation, Pagination]);
+
+
+
