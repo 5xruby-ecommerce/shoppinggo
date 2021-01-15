725d6a4420fbbe4dde03a510ae791c214df54684
ooo <ooo@gmail.com>
2021-01-15 18:24:27 +0800

    pages

--- app/javascript/scripts/swiper.js
+++ app/javascript/scripts/swiper.js
@@ -1,10 +1,18 @@
 import Swiper from 'swiper/bundle';
-console.log("hello")
+
 document.addEventListener('turbolinks:load', function(){
   var swiper = new Swiper('.swiper-container', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+    cssMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
 })
+
+  
