07d6d9808f063f553eb33964f20a9dd9be36bcaa
Henry <vinsen1109@gmail.com>
2021-01-19 10:31:46 +0800

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,16 @@
-ActiveRecord::Schema.define(version: 2021_01_13_075817) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_01_15_021627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +78,9 @@ ActiveRecord::Schema.define(version: 2021_01_13_075817) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "number"
+    t.string "receiver"
+    t.integer "tel"
+    t.string "address"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
