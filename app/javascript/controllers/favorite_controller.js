70ed22ddeefa46f20e3b7104930b3273c4a96d1c
kyu__yui <chiuyuf61139@gmail.com>
2021-01-22 17:54:29 +0800

    my_favorite_page can remove item

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -6,9 +6,9 @@ export default class extends Controller {
   static targets = [ "icon" ]
   
   go(e) {
+    const item = e.currentTarget.parentNode.parentNode.parentNode
     e.preventDefault();
     const id = this.data.get('id')
-
     Rails.ajax({
       url: `/products/${id}/favorite`,
       type: 'post',
@@ -16,9 +16,12 @@ export default class extends Controller {
         if (resp.status == "added") {
           this.iconTarget.classList.remove("far")
           this.iconTarget.classList.add("fas")
+          document.querySelector('#fav').textContent = '已喜歡'
         } else {
           this.iconTarget.classList.remove("fas")
           this.iconTarget.classList.add("far")
+          document.querySelector('#fav').textContent = '喜歡'
+          item.remove()
         }
       },
       error: function(err) {
@@ -26,4 +29,24 @@ export default class extends Controller {
       }
     })
   }
+
+
+  remove_fav(e) {
+    const item = e.currentTarget.parentNode.parentNode.parentNode
+    e.preventDefault();
+    const id = this.data.get('id')
+    Rails.ajax({
+      url: `/products/${id}/favorite`,
+      type: 'post',
+      success: (resp) => {
+        this.iconTarget.classList.remove("fas")
+        this.iconTarget.classList.add("far")
+        item.remove()    
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    })
+  }
+
 }
