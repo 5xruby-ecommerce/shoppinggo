9f96d76a4f3c4209b92f050ab9759903ad8f54dd
JiaMingXie <jiamingxie@JiaMingXiedeMacBook-Pro.local>
2021-01-24 18:55:35 +0800

    edit create product page fronted validate for both empty and number situation

--- app/javascript/scripts/product_validate.js
+++ app/javascript/scripts/product_validate.js
@@ -4,26 +4,46 @@ document.addEventListener('turbolinks:load', () => {
   const name = document.querySelector('#product_name');
   const price = document.querySelector('#product_price');
   const quantity = document.querySelector('#product_quantity');
+  const formbtn = document.querySelector('[type=submit]');
   const inputform = document.querySelector('#new_product');
   const validateinput = [name, price, quantity]
+  const validatenumber = [price, quantity]
   validateinput.forEach(element => {
     element.addEventListener('focusout', (e) => {
       const item = e.currentTarget
       if (item.value.trim() == '') {
         item.classList.add('invalid')
+        item.nextElementSibling.textContent = '不可為空白'
+        item.nextElementSibling.classList.add('error')
       } else {
         item.classList.remove('invalid')  
+        item.nextElementSibling.textContent = ''
+        item.nextElementSibling.classList.remove('error')
       }
     })
   });
 
-  inputform.addEventListener('click', (e)=> {
+  validatenumber.forEach(element => {
+    element.addEventListener('focusout', (e) => {
+      const item = e.currentTarget
+      if (isNaN(parseInt(item.value))) {
+        item.classList.add('invalid')
+        item.nextElementSibling.textContent = '須為數字'
+        item.nextElementSibling.classList.add('error')
+      }
+    })
+  })
+
+
+  formbtn.addEventListener('click', (e)=> {
     e.preventDefault()
 
     if (validateinput.map(el => el.value.trim()).includes('') || (validateinput.map(el => el.classList.contains('invalid'))).includes(true)) {
       validateinput.forEach(element => {
         if (element.value.trim() == '' || element.classList.contains('.invalid') == true) {
           element.classList.add('invalid')
+          element.nextElementSibling.textContent = '不可為空白'
+          element.nextElementSibling.classList.add('error')
         }
       });
     } else {
