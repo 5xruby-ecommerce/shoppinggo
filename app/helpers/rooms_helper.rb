e06fa79c6e1020c9232d2d90a2f8f20d76f20a99
kyu__yui <chiuyuf61139@gmail.com>
2021-01-26 13:25:32 +0800

    fix room bug

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,8 +1,10 @@
 module RoomsHelper
   def set_chatrooms
     if current_user.present?
+      if Room.participating(current_user).present?
         @rooms = Room.participating(current_user)
         render @rooms
+      end
     end
   end
 end
