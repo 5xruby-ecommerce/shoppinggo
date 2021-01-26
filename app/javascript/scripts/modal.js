4920559ce10989737d09f8a7b8b203903ecb2dfd
ooo <ooo@gmail.com>
2021-01-26 21:40:07 +0800

    modal js

--- app/javascript/scripts/modal.js
+++ app/javascript/scripts/modal.js
@@ -1,8 +1,35 @@
+import { resetWarningCache } from "prop-types"
+
 document.addEventListener('turbolinks:load', function(){
+  
+  const hello = document.cookie
+  const h = hello.split(';')
+  const o = h[1] ? h[1].split('=') : []
+  console.log(o)
+
+
+  if (o.length > 0 && o[1] == 1) {
+    document.querySelector('.modal').remove()
+    return
+  }
+ 
   if (document.querySelector('.X')) {
     document.querySelector('.X').addEventListener('click',()=>{
-      console.log('hell')
       document.querySelector('.modal').remove()
+      var expire_days = 1; // 過期日期(天)
+      var d = new Date();
+      d.setTime(d.getTime() + (expire_days * 24 * 60 * 60 * 1000));
+      var expires = "expires=" + d.toGMTString();
+      document.cookie = "modal=1" + "; " + expires + '; path=/';
+    
+
+      
+
     })  
   }
 })
+
+// 先顯示廣告
+// 使用者點擊按鈕後
+// 將此事件存進cookie
+// 過了一小時在cookie reset
